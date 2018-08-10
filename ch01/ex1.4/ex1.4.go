bd2605d5354cf58bc958b7147eedbffccd904467
Vadim Turkov <vadim.turkov@gmail.com>
2018-08-10 15:51:33 +0300

    ch01: add exercise 1.4 solution

---
+++ ch01/ex1.4/ex1.4.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	counts := make(map[string]int)
+	names := make(map[string]string)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		countLines(os.Stdin, counts, names)
+	} else {
+		for _, arg := range files {
+			f, err := os.Open(arg)
+			if err != nil {
+				fmt.Fprintf(os.Stderr, "dup2: %v\n", err)
+				continue
+			}
+			countLines(f, counts, names)
+			f.Close()
+		}
+	}
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\t%s\n", n, line, names[line])
+		}
+	}
+}
+
+func countLines(f *os.File, counts map[string]int, names map[string]string) {
+	input := bufio.NewScanner(f)
+	for input.Scan() {
+		text := input.Text()
+		counts[text]++
+		if !strings.Contains(names[text], f.Name()) {
+			names[text] += " " + f.Name()
+		}
+	}
+}
