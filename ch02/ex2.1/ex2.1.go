3333f66f5403dbf5fc3d1e9cf52c6b3dbab4271a
Vadim Turkov <vadim.turkov@gmail.com>
2018-08-14 10:42:12 +0300

    ch02: add exercise 2.1 solution

---
+++ ch02/ex2.1/ex2.1.go
@@ -0,0 +1,48 @@
+/*
+	Exercise 2.1:
+	Add types, constants, and functions to tempconv for processing temperatures in the Kelvin scale.
+*/
+
+package tempconv
+
+import "fmt"
+
+type Celsius float64
+type Fahrenheit float64
+type Kelvin float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+
+	AbsoluteZeroF Fahrenheit = -459.67
+	FreezingF     Fahrenheit = 32
+	BoilingF      Fahrenheit = 212
+
+	AbsoluteZeroK Kelvin = 0
+	FreezingK     Kelvin = 273.15
+	BolingK       Kelvin = 373.15
+)
+
+func (c Celsius) String() string    { return fmt.Sprintf("%g°C", c) }
+func (f Fahrenheit) String() string { return fmt.Sprintf("%g°F", f) }
+func (k Kelvin) String() string     { return fmt.Sprintf("%gK", k) }
+
+// CToF converts a Celsius temperature to Fahrenheit.
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+
+// CToK converts a Celsius temperature to Kelvin.
+func CToK(c Celsius) Kelvin { return Kelvin(c + 273.15) }
+
+// FToC converts a Fahrenheit temperature to Celsius.
+func FToC(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
+
+// FToK converts a Fahrenheit temperature to Kelvin.
+func FToK(f Fahrenheit) Kelvin { return Kelvin((f + 459.67) * 5 / 9) }
+
+// KToC converts a Kelvin temperature to Celsius.
+func KToC(k Kelvin) Celsius { return Celsius(k - 273.15) }
+
+// KToF converts a Kelvin temperature to Fahrenheit.
+func KToF(k Kelvin) Fahrenheit { return Fahrenheit(k*9/5 - 459.67) }
