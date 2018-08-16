99e0ef753d3f5412353c49e818a6c27bde9fee46
Vadim Turkov <vadim.turkov@gmail.com>
2018-08-16 11:06:54 +0300

    ch03: update exercise 3.1 solution

--- ch03/ex3.1/ex3.1.go
+++ ch03/ex3.1/ex3.1.go
@@ -10,15 +10,15 @@ import (
 )
 
 const (
-	width, height = 600, 320            // canvas size in pixels
-	cells         = 100                 // number of grid cells
-	xyrange       = 30.0                // axis ranges (-xyrange..+xyrange)
-	xyscale       = width / 2 / xyrange // pixels per x or y unit
-	zscale        = height * 0.4        // pixels per z unit
-	angle         = math.Pi / 6         // angle of x, y axes (=30°)
+	width, height = 600, 320
+	cells         = 30
+	xyrange       = 30.0
+	xyscale       = width / 2 / xyrange
+	zscale        = height * 0.4
+	angle         = math.Pi / 6
 )
 
-var sin30, cos30 = math.Sin(angle), math.Cos(angle) // sin(30°), cos(30°)
+var sin30, cos30 = math.Sin(angle), math.Cos(angle)
 
 func main() {
 	fmt.Printf("<svg xmlns='http://www.w3.org/2000/svg' "+
@@ -38,21 +38,18 @@ func main() {
 }
 
 func corner(i, j int) (float64, float64) {
-	// Find point (x,y) at corner of cell (i,j).
 	x := xyrange * (float64(i)/cells - 0.5)
 	y := xyrange * (float64(j)/cells - 0.5)
 
-	// Compute surface height z.
 	z := f(x, y)
 
-	// Project (x,y,z) isometrically onto 2-D SVG canvas (sx,sy).
 	sx := width/2 + (x-y)*cos30*xyscale
 	sy := height/2 + (x+y)*sin30*xyscale - z*zscale
 	return sx, sy
 }
 
 func f(x, y float64) float64 {
-	r := math.Hypot(x, y) // distance from (0,0)
+	r := math.Hypot(x, y)
 	if r == 0 {
 		return 1
 	}
