ecdf8167da9ad4c1f3a4111e6677b02f4324f751
Vadim Turkov <vadim.turkov@gmail.com>
2018-08-16 11:22:17 +0300

    ch03: update cells number in exercise 3.1 solution

--- ch03/ex3.1/ex3.1.go
+++ ch03/ex3.1/ex3.1.go
@@ -11,7 +11,7 @@ import (
 
 const (
 	width, height = 600, 320
-	cells         = 30
+	cells         = 100
 	xyrange       = 30.0
 	xyscale       = width / 2 / xyrange
 	zscale        = height * 0.4
