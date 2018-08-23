e5363c17f67f0a2be9e9b39acffcc1dfe48cc51f
Vadim Turkov <vadim.turkov@gmail.com>
2018-08-23 12:55:36 +0300

    ch03: add exercise 3.13 solution

---
+++ ch03/ex3.13/ex3.13.go
@@ -0,0 +1,16 @@
+/*
+	Exercise 3.12
+*/
+
+package byteconst
+
+const (
+	KB = 1000
+	MB = 1000 * KB
+	GB = 1000 * MB
+	TB = 1000 * GB
+	PB = 1000 * TB
+	EB = 1000 * PB
+	ZB = 1000 * EB
+	YB = 1000 * ZB
+)
